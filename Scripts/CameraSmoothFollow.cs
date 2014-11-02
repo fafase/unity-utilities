7c7e5bc83dee3c1b5e0f2c8a2ba5a7c427a00bba
Lucas <lucas.cosson@metropolia.com>
2014-11-02 19:48:46 +0200

    Adding State, Audio, Camera

---
+++ Scripts/CameraSmoothFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSmoothFollow : MonoBehaviour {
+
+	[SerializeField] private float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	[SerializeField] private Transform target;
+    [SerializeField]
+    private float xMin = 0.0f, xMax = 0.0f, yMin = 0.0f, yMax = 0.0f;
+
+	void Update () 
+	{
+		if (target)
+		{
+			Vector3 point = camera.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+            destination.x = Mathf.Clamp(destination.x,xMin, xMax);
+            destination.y = Mathf.Clamp(destination.y ,yMin, yMax);
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);          
+		}
+	}
+}
