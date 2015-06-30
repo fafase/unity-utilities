6a035f1e5865d38ad0a6d333eee1d6489eadf8a1
Lucas <lucascoss@gmail.com>
2015-06-30 14:33:15 +0300

    Update ObjectPool.cs

--- Scripts/ObjectPool.cs
+++ Scripts/ObjectPool.cs
@@ -1,134 +1,35 @@
-using UnityEngine;
-using System.Collections.Generic;
+	public GameObject prefab;
+	public GameObject prefabB;
+	public Transform tr, trB;
+	private ObjectPool objectPool = null;
+	void Start () 
+	{
+		objectPool = ObjectPool.Instance;
+		objectPool.AddToPool(prefab,20,tr);	
+		objectPool.AddToPool(prefabB,5,trB);	
+	}
 
-/// <summary>
-/// Object pool.
-/// Basic example:
-/// 
-/// [SerializeField] private GameObject enemyPrefab = null;
-/// [SerializeField] private GameObject bulletPrefab = null;
-/// [SerializeField] private Transform enemyContainer = null;
-/// [SerializeField] private Transform weaponContainer = null;
-/// void Start()
-/// {
-/// 	ObjectPool.Instance.AddToPool(enemyPrefab, 10 , enemyContainer);
-/// 	ObjectPool.Instance.AddToPool(bulletPrefab, 100, weaponContainer);
-/// }
-/// 
-/// void Shoot()
-/// {
-/// 	GameObject bullet = ObjectPool.Instance.PopFromPool(bulletPrefab);
-/// }
-/// 
-/// void Hit(GameObject bullet)
-/// {
-///     ObjectPool.Instance.PushToPool(ref bulletPrefab, ref bullet, bulletContainer);
-/// }
-/// </summary>
-public sealed class ObjectPool
-{
-	private Dictionary <GameObject, List<GameObject>> container = new Dictionary<GameObject, List<GameObject>>();
-
-	private static ObjectPool instance = null;
-	public static ObjectPool Instance {
-		get
+	List<GameObject>list = new List<GameObject>();
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
 		{
-			if (instance==null)
+			GameObject obj = objectPool.PopFromPool(prefab);	
+			if(obj != null)
 			{
-				instance = new ObjectPool();
-			}
-			return instance;
-		}
-	}
-
-    public void Reset() {
-        instance = null;
-    }
-
-	private ObjectPool() {}    
-
-	private List<GameObject> FindInContainer(ref GameObject prefab)
-	{
-		if(container.ContainsKey(prefab) == false)
-		{
-			container.Add(prefab, new List<GameObject>());
-		}
-		return container[prefab];
-	}
-	
-	public void AddToPool(GameObject prefab, int count, Transform parent = null)
-	{
-		if(prefab == null || count <= 0)
-		{
-			return;
-		}
-		for (int i = 0; i < count ; i++)
-		{
-			GameObject obj = PopFromPool(prefab, true);
-			PushToPool(ref prefab,ref obj, parent);
-		}
-	}
-
-	public GameObject PopFromPool(GameObject prefab, bool forceInstantiate = false)
-	{
-		if(prefab == null)
-		{
-			return null;
-		} 
-		GameObject o = null;
-		if( forceInstantiate == true)
-		{
-			o = (GameObject)Object.Instantiate(prefab);
-			return o;
-		}
-		List<GameObject> list = FindInContainer(ref prefab);
-		if(list.Count > 0)
-		{
-			o = list[0];
-			list.RemoveAt(0);
-			o.SetActive(true);
-		}
-		return o;
-	}
-
-	public void PushToPool(ref GameObject prefab, ref GameObject obj, Transform parent = null)
-	{
-		if(prefab == null||obj == null)
-		{
-			return;
-		}
-		if(parent != null)
-		{
-			obj.transform.parent = parent;
-		}
-		List<GameObject> list = FindInContainer(ref prefab);
-		list.Add (obj);
-		obj.SetActive(false);
-	}
-
-	public void ReleaseItems(ref GameObject prefab, ref GameObject obj)
-	{
-		if(prefab == null || obj == null)
-		{
-			return;
-		}
-		var list = FindInContainer(ref prefab);
-		int index = list.IndexOf(obj);
-		list.RemoveAt (index);
-		Object.Destroy(obj);
-	}
-
-	public void ReleasePool()
-	{
-		foreach (var kvp in container)
-		{
-			var list = kvp.Value;
-			foreach(var obj in list)
-			{
-				Object.Destroy(obj);
+				obj.name = "Found"; 
+				list.Add (obj);
 			}
 		}
-		container = null;
-		container = new Dictionary<GameObject, List<GameObject>>();
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			if(list.Count > 0)
+			{
+				int index = Random.Range(0, list.Count - 1);
+				GameObject obj = list[index];
+				list.RemoveAt(index);
+				objectPool.PushToPool(prefab,obj,tr);
+			}
+		}
 	}
-}
