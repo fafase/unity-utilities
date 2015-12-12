339bba117a62723348aa3e506d7e99c5ca7e2fb7
Lucas <lucascoss@gmail.com>
2015-12-12 19:58:32 +0200

    Update ObjectPool.cs

--- Scripts/ObjectPool.cs
+++ Scripts/ObjectPool.cs
@@ -3,136 +3,132 @@ using System.Collections.Generic;
 using UnityEngine;
 public sealed class ObjectPool
 {
-    private Dictionary<GameObject, Queue<GameObject>> container = new Dictionary<GameObject, Queue<GameObject>>();
+	private Dictionary<GameObject, Queue<GameObject>> container = new Dictionary<GameObject, Queue<GameObject>>();
 	
-    private static ObjectPool instance = null;
-    public static ObjectPool Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                instance = new ObjectPool();
-            }
-            return instance;
-        }
-    }
-    public void Reset()
-    {
-        instance = null;
-    }
-    private ObjectPool() { }
-	
-    public bool AddToPool(GameObject prefab, int count, Transform parent = null) 
-    {
-        if (prefab == null || count <= 0) { return false; }
-        string name = prefab.name;
-        for (int i = 0; i < count; i++)
-        {
-            GameObject obj = PopFromPool(prefab, true);
-            PushToPool(ref obj, true, parent);
-        }
-        return true;
-    }
-	
-    public GameObject PopFromPool(GameObject prefab, bool forceInstantiate = false, bool instantiateIfNone = false, Transform container = null)
-    {
-	GameObject obj = null;
-	if (forceInstantiate == true) 
-	{ 
-		obj =  CreateObject(prefab, container); 
-	}
-	Queue<GameObject> queue = FindInContainer(prefab);
-	if (queue.Count > 0)
+	private static ObjectPool instance = null;
+	public static ObjectPool Instance
 	{
-		obj = queue.Dequeue();
-		obj.SetActive(true);
+		get
+		{
+			if (instance == null)
+			{
+				instance = new ObjectPool();
+			}
+			return instance;
+		}
+	}
+	public void Reset()
+	{
+		instance = null;
+	}
+	private ObjectPool() { }
+	
+	public bool AddToPool(GameObject prefab, int count, Transform parent = null) 
+	{
+		if (prefab == null || count <= 0) { return false; }
+		for (int i = 0; i < count; i++)
+		{
+			GameObject obj = PopFromPool(prefab, true, false, parent);
+			PushToPool(ref obj, true, parent);
+		}
+		return true;
+	}
+	
+	public GameObject PopFromPool(GameObject prefab, bool forceInstantiate = false, bool instantiateIfNone = false, Transform container = null)
+	{
+		GameObject obj = null;
+		if (forceInstantiate == true) { 
+			obj = CreateObject (prefab, null); 
+		} else {
+			Queue<GameObject> queue = FindInContainer (prefab);
+			if (queue.Count > 0) {
+				obj = queue.Dequeue ();
+				obj.SetActive (true);
+				obj.transform.parent = container;
+			}
+		}
+		if (obj == null && instantiateIfNone == true)
+		{
+			obj = CreateObject(prefab, container);
+		}
+		obj.GetComponent<IPoolObject> ().Init ();
+		return obj;
+	}
+	private Queue<GameObject> FindInContainer(GameObject prefab)
+	{
+		if (container.ContainsKey(prefab) == false)
+		{
+			container.Add(prefab, new Queue<GameObject>());
+		}
+		return container[prefab];
+	}
+	private GameObject CreateObject(GameObject prefab, Transform container)
+	{
+		IPoolObject poolObjectPrefab = prefab.GetComponent<IPoolObject>();
+		if(poolObjectPrefab== null){Debug.Log ("Wrong type of object"); return null;}
+		
+		GameObject obj = (GameObject)Object.Instantiate(prefab);
+		IPoolObject poolObject = obj.GetComponent<IPoolObject>();
+		obj.name = prefab.name;
+		poolObject.Prefab = prefab;	
 		obj.transform.parent = container;
+		return obj;
 	}
-	if (obj == null && instantiateIfNone == true)
-	{
-		obj = CreateObject(prefab, container);
-	}
-	obj.GetComponent<IPoolObject> ().Init ();
-	return obj;
-    }
-    private Queue<GameObject> FindInContainer(GameObject prefab)
-    {
-        if (container.ContainsKey(prefab) == false)
-        {
-            container.Add(prefab, new Queue<GameObject>());
-        }
-        return container[prefab];
-    }
-    private GameObject CreateObject(GameObject prefab, Transform container)
-    {
-        IPoolObject poolObjectPrefab = prefab.GetComponent<IPoolObject>();
-        if(poolObjectPrefab== null){Debug.Log ("Wrong type of object"); return null;}
-
-        GameObject obj = (GameObject)Object.Instantiate(prefab);
-        IPoolObject poolObject = obj.GetComponent<IPoolObject>();
-        obj.name = prefab.name;
-        poolObject.Prefab = prefab;
-        obj.transform.parent = container;
-
-        return obj;
-    }
 	
-    public void PushToPool(ref GameObject obj, bool retainObject = true, Transform parent = null)
-    {
-        if (obj == null) { return; }
-        if (retainObject == false)
-        {
-            Object.Destroy(obj);
-            obj = null;
-            return;
-        }
-        if (parent != null)
-        {
-            obj.transform.parent = parent;
-        }
-        IPoolObject poolObject = obj.GetComponent<IPoolObject>();
-        GameObject prefab = null;
-        if(poolObject != null)
-        {
-            prefab = poolObject.Prefab;
-            Queue<GameObject> queue = FindInContainer(prefab);
-            queue.Enqueue(obj);
-            obj.SetActive(false);
-         }
-         obj = null;
-    }
-    public void ReleaseItems(GameObject prefab, bool destroyObject = false)
-    {
-        if (prefab == null) { return; }
-        Queue<GameObject> queue = FindInContainer(prefab);
-        if (queue == null) { return; }
-        while (queue.Count > 0)
-        {
-            GameObject obj = queue.Dequeue();
-            if (destroyObject == true)
-            {
-                Object.Destroy(obj);
-            }
-        }
-    }
-    public void ReleasePool() 
-    {
-        foreach (var kvp in container)
-        {
-            Queue<GameObject> queue = kvp.Value;
-            while (queue.Count > 0)
-            {
-                GameObject obj = queue.Dequeue();
-                Object.Destroy(obj);
-            }
-         }
-         container = null;
-         container = new Dictionary<GameObject, Queue<GameObject>>();
-    }
+	public void PushToPool(ref GameObject obj, bool retainObject = true, Transform newParent = null)
+	{
+		if (obj == null) { return; }
+		if (retainObject == false)
+		{
+			Object.Destroy(obj);
+			obj = null;
+			return;
+		}
+		if (newParent != null)
+		{
+			obj.transform.parent = newParent;
+		}
+		IPoolObject poolObject = obj.GetComponent<IPoolObject>();
+		if(poolObject != null)
+		{
+			GameObject prefab = poolObject.Prefab;
+			Queue<GameObject> queue = FindInContainer(prefab);
+			queue.Enqueue(obj);
+			obj.SetActive(false);
+		}
+		obj = null;
+	}
+	public void ReleaseItems(GameObject prefab, bool destroyObject = false)
+	{
+		if (prefab == null) { return; }
+		Queue<GameObject> queue = FindInContainer(prefab);
+		if (queue == null) { return; }
+		while (queue.Count > 0)
+		{
+			GameObject obj = queue.Dequeue();
+			if (destroyObject == true)
+			{
+				Object.Destroy(obj);
+			}
+		}
+	}
+	public void ReleasePool() 
+	{
+		foreach (var kvp in container)
+		{
+			Queue<GameObject> queue = kvp.Value;
+			while (queue.Count > 0)
+			{
+				GameObject obj = queue.Dequeue();
+				Object.Destroy(obj);
+			}
+		}
+		container = null;
+		container = new Dictionary<GameObject, Queue<GameObject>>();
+	}
 }
 public interface IPoolObject
 {
-    GameObject Prefab{get;set;}
-    void Init();
+	GameObject Prefab{get;set;}
+	void Init();
 }
